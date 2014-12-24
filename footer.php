f6a13ecbba5d65abe60ffa6615d7c93265a61059
m-e-h <info@martyhelmick.com>
2014-12-23 23:59:42 -0500

    style footer

--- footer.php
+++ footer.php
@@ -9,17 +9,18 @@
 ?>
 
 	</div><!-- #content -->
-
+	
+  <footer <?php hybrid_attr( 'footer' ); ?>>
+    
 	<?php hybrid_get_sidebar( 'subsidiary' ); ?>
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
+	
 		<div class="site-info">
 			<?php printf(
 					__( 'Copyright &#169; %1$s %2$s. Powered by %3$s and %4$s.', 'saga' ), 
 					date_i18n( 'Y' ), hybrid_get_site_link(), hybrid_get_wp_link(), hybrid_get_theme_link()
 						); ?>
 		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+	</footer><!-- #footer -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>

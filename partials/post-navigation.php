273f5acbc183ebaedf8aca12cb4bae7ddcda5d0b
m-e-h <info@martyhelmick.com>
2015-01-13 13:31:44 -0500

    post navigation and some random formating

--- partials/post-navigation.php
+++ partials/post-navigation.php
@@ -2,11 +2,11 @@
 			return;
 	}
 
-		the_post_navigation( array(
-			'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( '&larr;', 'scratch' ) . '</span> ' .
-				'<span class="screen-reader-text">' . __( 'Previous article:', 'scratch' ) . '</span> ' .
-				'<span class="post-title">%title</span>',
-			'next_text' => '<span class="screen-reader-text">' . __( 'Next article:', 'scratch' ) . '</span>
-				<span class="post-title">%title</span>
-				<span class="meta-nav" aria-hidden="true">' . __( '&rarr;', 'scratch' ) . '</span> ',
-		) );
+			the_post_navigation( array(
+				'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next &rarr;', 'twentyfifteen' ) . '</span> ' .
+					'<span class="screen-reader-text">' . __( 'Next post:', 'twentyfifteen' ) . '</span> ' .
+					'<span class="post-title">%title</span>',
+				'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( '&larr; Previous', 'twentyfifteen' ) . '</span> ' .
+					'<span class="screen-reader-text">' . __( 'Previous post:', 'twentyfifteen' ) . '</span> ' .
+					'<span class="post-title">%title</span>',
+			) );
